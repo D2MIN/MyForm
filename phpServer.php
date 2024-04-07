7e5ba25a259f8090c94165b67ab721c0af71785f
D2MIN <aamor8448@gmail.com>
2024-04-07 17:18:51 +0300

    Test_PHP_server

---
+++ phpServer.php
@@ -0,0 +1,9 @@
+<?php 
+    echo $_POST['name'];
+    echo $_POST['number'];
+    echo $_POST['email'];
+    echo $_POST['date'];
+    echo $_POST['gen'];
+    echo $_POST['leng[]'];
+    echo $_POST['about'];
+?>
