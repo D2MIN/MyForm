56bf58698c45aadcd65f507f09af8bef3242cc52
D2MIN <aamor8448@gmail.com>
2024-04-07 17:42:39 +0300

    C3

--- phpServer.php
+++ phpServer.php
@@ -1,9 +1,3 @@
 <?php
-if (isset($_POST['name'])) {
-    $name = $_POST['name'];
-    $message = $_POST['message'];
-
-    echo "Name: $name<br>";
-    echo "Message: $message<br>";
-  }
+phpinfo();
 ?>
