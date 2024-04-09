d4e05740d4b55a5d8d0a981cca233f1621438a50
D2MIN <aamor8448@gmail.com>
2024-04-09 14:22:08 +0300

    Tets_Cookie

--- phpServer.php
+++ phpServer.php
@@ -1,3 +1,10 @@
 <?php
-    echo "Hello world it is PHP";
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $name = $_POST['name'];
+        if($name == 'Roman'){
+            setcookie("name",$name, time() + 86400,"/");
+        }else{
+            $nameErr = "invalid Name";
+        }
+    }
 ?>
