50c90aadf4473447e5969dcdff725b321ad462f2
D2MIN <aamor8448@gmail.com>
2024-04-10 21:23:02 +0300

    10.04.2024

--- index.php
+++ index.php
@@ -56,9 +56,9 @@
             <div class="info">
                 <div class="input">
                     <input class="<?php echo $_COOKIE['nameErr'] ?>" name="name" id="name" type="text" value="<?php echo $_COOKIE["name"]; ?>" placeholder="Имя" required>
-                        <span class="span <?php $_COOKIE['nameErr'] ?>"> <?php if(isset($_COOKIE['nameErr'])) echo "Неверные символы" ?> </span>
-                    <input class="<?php echo $numberErr?>" name="number" id="number" type="number" value="<?php echo $_COOKIE["number"]; ?>" placeholder="Номер" required>
-                        <span class="span <?php echo $numberErr?>"> <?php if($numberErr != 0) echo "Неправильное количество цифр" ?> </span>
+                        <span class="span <?php echo $_COOKIE['nameErr'] ?>"> <?php if(isset($_COOKIE['nameErr'])) echo "Неверные символы" ?> </span>
+                    <input class="<?php echo $_COOKIE['numberErr'] ?>" name="number" id="number" type="number" value="<?php echo $_COOKIE["number"]; ?>" placeholder="Номер" required>
+                        <span class="span <?php echo $_COOKIE['numberErr'] ?>"> <?php if($numberErr != 0) echo "Неправильное количество цифр" ?> </span>
                     <input name="email" id="email" type="email" value="<?php echo $_COOKIE["email"]; ?>" placeholder="Почта" required>
                     <input name="date" id="date" type="date" placeholder="" required>
                 </div>
