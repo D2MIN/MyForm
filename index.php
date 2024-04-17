61c1390fc21eae7f794615c8a4c6f9a4fb332d5c
D2MIN <aamor8448@gmail.com>
2024-04-17 20:31:50 +0300

    17apr

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
             $result = file_get_contents($url, false, $context);
             $answer = "Данные отправлены!";
             $answer = urlencode($answer);
-            header("Location: index.php?answer=".$lengs);
+            header("Location: index.php?answer=".$answer);
         }
         
     }
