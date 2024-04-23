6d16e73a880541b19e9b86b7fd859b547756fdde
D2MIN <aamor8448@gmail.com>
2024-04-23 18:37:01 +0300

    GetLoginPHP

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <div class="content">
         <div class="loginForm">
             <h1>Войдите что бы менять таблицу</h1>
-            <form action="">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" >
                 <input name="name" type="text" value="<?php echo $username; ?>">
                 <input name="password" type="password">
                 <div class="buttons">
