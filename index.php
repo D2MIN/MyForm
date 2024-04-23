39dc262347efeffa83671473c376b6c4359d5a73
D2MIN <aamor8448@gmail.com>
2024-04-23 18:32:58 +0300

    GetLoginPHP

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['name'];
     $password = $_POST['password'];
 
-    if ($username == 'admin' && $password == 'admin') {
-        header('Location: changeForm.php');
+    if ($username == "admin" && $password == "admin") {
+        header('Location: /');
     }
 }
 ?>
