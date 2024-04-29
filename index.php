769ef8ede73b3cd7ffee3458d5b18bf189e6134b
D2MIN <aamor8448@gmail.com>
2024-04-29 15:12:26 +0300

    25.04.2024;

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     mysqli_set_charset($db, 'utf8');
 
     //Запрос к базе данных
-    $result = $db->query("SELECT pass FROM users WHERE login = '$login'");
+    $result = $db->query("SELECT pass FROM users WHERE login = '$pass'");
     // if ($result->num_rows > 0) {
     //     $row = $result->fetch_assoc();
     //     $pass = $row['pass'];
