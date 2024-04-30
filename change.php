36fd64a2c02aa0ddb2365895b3ae8db1e14ae361
D2MIN <aamor8448@gmail.com>
2024-04-30 13:07:43 +0300

    30.04.2024

--- change.php
+++ change.php
@@ -62,7 +62,8 @@
             $gen = mysqli_real_escape_string($db, $gen);
             $about = mysqli_real_escape_string($db, $about);
             $id = mysqli_real_escape_string($db, $id);
-            $db->query("UPDATE users SET name='$name', number='$number', email='$email', date='$date', gen='$gen', about='$about' WHERE id = $id");
+            $db->query("UPDATE users SET name='$name' WHERE id = $id");
+            //, number='$number', email='$email', date='$date', gen='$gen', about='$about'
 
             $answer = "Форма успешно изменена !";
             header("Location: change.php?answer=".$answer."&pasword=".$password."&id=".$id);
