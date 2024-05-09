7d72a9529199c2be63cfdc4cd44e67525f23b333
D2MIN <aamor8448@gmail.com>
2024-05-09 17:04:00 +0300

    9_may_2024

--- lengStatus.php
+++ lengStatus.php
@@ -6,22 +6,22 @@
     }
     mysqli_set_charset($db, 'utf8');
 
-    $result = db->qery("select count(*) from user_lengs where leng_id = '1'");
+    $result = $db->qery("select count(*) from user_lengs where leng_id = '1'");
     echo $result;
-    
-    $lengs = array(
-        "1" => 0,
-        "2" => 0,
-        "3" => 0,
-        "4" => 0,
-        "5" => 0,
-        "6" => 0,
-        "7" => 0,
-        "8" => 0,
-        "9" => 0,
-        "10" => 0,
-        "11" => 0,
-    );
+
+    // $lengs = array(
+    //     "1" => 0,
+    //     "2" => 0,
+    //     "3" => 0,
+    //     "4" => 0,
+    //     "5" => 0,
+    //     "6" => 0,
+    //     "7" => 0,
+    //     "8" => 0,
+    //     "9" => 0,
+    //     "10" => 0,
+    //     "11" => 0,
+    // );
 ?>
 
 <!DOCTYPE html>
