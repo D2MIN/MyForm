a21176a1080070dab9e67880ec043487a6ec93b9
D2MIN <aamor8448@gmail.com>
2024-05-10 19:51:23 +0300

    10.may.2024

---
+++ allTable.php
@@ -0,0 +1,29 @@
+<?php
+    $db = mysqli_connect('localhost', 'd2min', 'Qwerty40982', 'Form');
+    if (!$db) {
+        die('Error connecting to database: ' . mysqli_connect_error());
+    }
+    $result = $db->query("SELECT * FROM users");
+    // $row = mysqli_fethc_assoc($result);
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo '<tr>';
+        echo '<td>' . $row['column_name1'] . '</td>';
+        echo '<td>' . $row['column_name2'] . '</td>';
+        echo '<td>' . $row['column_name3'] . '</td>';
+        echo '</tr>';
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All Tables</title>
+</head>
+<body>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -71,6 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             </form>
             <a href="http://95.213.139.91/MyForm/form.php"><button>Я новый пользователь</button></a>
             <a href="http://95.213.139.91/MyForm/lengStatus.php"><button>Посмотреть статиcтику</button></a>
+            <a href="http://95.213.139.91/MyForm/allTable.php"><button>Посмотреть таблицу</button></a>
         </div>
     </div>
 </body>
