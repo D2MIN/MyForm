7448af20f7d057f26f80077942ce99ffebb4a0ef
D2MIN <aamor8448@gmail.com>
2024-05-10 20:03:40 +0300

    10.may.2024

--- lengStatus.php
+++ lengStatus.php
@@ -33,7 +33,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="lengStatus.css">
+    <link rel="stylesheet" href="css/lengStatus.css">
     <title>LengStatus</title>
 </head>
 <body>
