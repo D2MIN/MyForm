521be427bc996f9e67965af5c6294fd709b02855
D2MIN <aamor8448@gmail.com>
2024-05-12 19:44:22 +0300

    12.May.2004

--- admin.js
+++ admin.js
@@ -1,1 +1,8 @@
 console.log("Hello admin");
+
+let chBtn = document.getElementById("change");
+let dlBtn = document.getElementById("delete");
+
+chBtn.onclick("click",()=>{
+    window.location.href = 'index.php';
+});
