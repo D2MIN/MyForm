1618ff0d9bc90ae7b5a0d574528a40e655b8a274
D2MIN <aamor8448@gmail.com>
2024-05-12 19:49:00 +0300

    12.May.2004

--- admin.js
+++ admin.js
@@ -3,6 +3,9 @@ console.log("Hello admin");
 let chBtn = document.getElementsByClassName("change");
 let dlBtn = document.getElementsByClassName("delete");
 
+console.log(chBtn.value);
+console.log(dlBtn);
+
 chBtn.onclick = function() {
     console.log("ch");
     window.location.href = 'index.php';
