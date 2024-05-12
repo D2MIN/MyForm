6737e69c9070b2ff723f0cda8600042e9c26c832
D2MIN <aamor8448@gmail.com>
2024-05-12 19:55:21 +0300

    12.May.2004

--- admin.js
+++ admin.js
@@ -1,9 +1,14 @@
 console.log("Hello admin");
 
-let chBtn = document.getElementsByClassName("change")[0];
-let dlBtn = document.getElementsByClassName("delete")[0];
+let chBtn = document.getElementById("change");
+let dlBtn = document.getElementById("delete");
 
 chBtn.onclick = function() {
     console.log("ch");
+    window.location.href = 'index.php';
+};
+
+dlBtn.onclick = function() {
+    console.log("dl");
     window.location.href = 'index.php';
 };
