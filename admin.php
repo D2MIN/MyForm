1f8de750f316f45d014005ccb70854aaef4201b4
D2MIN <aamor8448@gmail.com>
2024-05-13 17:43:57 +0300

    13.May.2024

--- admin.php
+++ admin.php
@@ -5,29 +5,33 @@ if (!$db) {
 }
 mysqli_set_charset($db, 'utf8');
 
-// Получаем имя пользователя и пароль из заголовка Authorization
-$auth = $_SERVER['HTTP_AUTHORIZATION'];
-$auth = base64_decode(substr($auth, 6));
-list($username, $pass) = explode(':', $auth);
 
-$result = $db->query("SELECT * FROM admins WHERE login = '$username'");
-if (!$db) {
-    die('Error connecting to database: ' . mysqli_connect_error());
-}
-$row = $result->fetch_assoc();
-$login = $row['login'];
-$password = $row['pass'];
+if ($_SERVER['PHP_AUTH_USER'] != "" || $_SERVER['PHP_AUTH_PW'] != ""){
 
+    // Получаем имя пользователя и пароль из заголовка Authorization
+    $auth = $_SERVER['HTTP_AUTHORIZATION'];
+    $auth = base64_decode(substr($auth, 6));
+    list($username, $pass) = explode(':', $auth);
 
-if ($username != $login || $pass != $password || $username == "" || $pass == ""){
+    $result = $db->query("SELECT * FROM admins WHERE login = '$username'");
+    $row = $result->fetch_assoc();
+    $login = $row['login'];
+    $password = $row['pass'];
+
+    if($username != $login || $pass != $password){
+        header('WWW-Authenticate: Basic realm="Restricted Area"');
+        header('HTTP/1.0 401 Unauthorized');
+        die("Пожалуйста введите свой логин и пароль");
+    }
+    else{
+        $flag = "True";
+        session_start();
+        $_SESSION['admin'] = $flag;
+        header("Location: allTable.php");
+    }
+}else{
     header('WWW-Authenticate: Basic realm="Restricted Area"');
     header('HTTP/1.0 401 Unauthorized');
     die("Пожалуйста введите свой логин и пароль");
-}
-else{
-    $flag = "True";
-    session_start();
-    $_SESSION['admin'] = $flag;
-    header("Location: allTable.php");
 }
 ?>
