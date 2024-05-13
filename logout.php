222887b1a35c3296ec0b28e9a309ff8821c98c4c
D2MIN <aamor8448@gmail.com>
2024-05-13 17:45:50 +0300

    13.May.2024

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy();
-unset($_SERVER['HTTP_AUTHORIZATION']);
+$_SERVER['PHP_AUTH_USER'] = "";
 header("Location: index.php"); // перенаправление на главную страницу после выхода
 ?>
