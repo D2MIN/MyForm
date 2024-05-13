c9f61799a87635b695b7dc500ebc0b14d924199e
D2MIN <aamor8448@gmail.com>
2024-05-13 16:53:02 +0300

    13.May.2024

--- admin.js
+++ admin.js
@@ -3,7 +3,8 @@ console.log("Hello admin");
 let chBtn = document.getElementById("change");
 let dlBtn = document.getElementById("delete");
 
-let id = document.getElementById("id").value;
+let id = document.getElementById("id");
+console.log(id);
 
 chBtn.onclick = function() {
     window.location.href = 'adminChange.php?id='+id;
