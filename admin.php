a59fd94b840759341f8b54b6e60a7387d8aa9500
D2MIN <aamor8448@gmail.com>
2024-05-13 17:39:02 +0300

    13.May.2024

--- admin.php
+++ admin.php
@@ -15,11 +15,11 @@ $row = $result->fetch_assoc();
 $login = $row['login'];
 $password = $row['pass'];
 
-$txt = "|end";
-if ($_SERVER['PHP_AUTH_USER'] != $login || $_SERVER['PHP_AUTH_PW'] != $password || $_SERVER['PHP_AUTH_USER'] == "" || $_SERVER['PHP_AUTH_PW'] == ""){
+
+if ($username != $login || $pass != $password){
     header('WWW-Authenticate: Basic realm="Restricted Area"');
     header('HTTP/1.0 401 Unauthorized');
-    die("Пожалуйста введите свой логин и пароль".$username.$pass.$txt);
+    die("Пожалуйста введите свой логин и пароль");
 }
 else{
     $flag = "True";
