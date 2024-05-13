921247e7010f5e8570a4382a6e04503fae418e95
D2MIN <aamor8448@gmail.com>
2024-05-13 17:53:09 +0300

    13.May.2024

--- logout.php
+++ logout.php
@@ -2,6 +2,8 @@
 session_start();
 $_SESSION = array();
 session_destroy();
+header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+header('Pragma: no-cache');
 header('Authorization:');
 header('WWW-Authenticate: Basic realm=""');
 header("Location: index.php"); // перенаправление на главную страницу после выхода
