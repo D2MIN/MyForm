beb839d4c88c9b9332e2ca31e6251307f48de087
D2MIN <aamor8448@gmail.com>
2024-05-13 17:54:03 +0300

    13.May.2024

--- logout.php
+++ logout.php
@@ -2,7 +2,8 @@
 session_start();
 $_SESSION = array();
 session_destroy();
-header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Pragma: no-cache');
 header('Authorization:');
 header('WWW-Authenticate: Basic realm=""');
