6438820c30c51b48368236f8c8470cbd8e523f96
D2MIN <aamor8448@gmail.com>
2024-05-13 16:36:50 +0300

    13.May.2024

--- admin.js
+++ admin.js
@@ -4,11 +4,9 @@ let chBtn = document.getElementById("change");
 let dlBtn = document.getElementById("delete");
 
 chBtn.onclick = function() {
-    console.log("ch");
     window.location.href = 'index.php';
 };
 
 dlBtn.onclick = function() {
-    console.log("dl");
     window.location.href = 'index.php';
 };
