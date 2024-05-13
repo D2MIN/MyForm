5f4d20468f7974db7c80ac12a06bf03fe9bab482
D2MIN <aamor8448@gmail.com>
2024-05-13 18:00:43 +0300

    13.May.2024

--- logout.php
+++ logout.php
@@ -2,7 +2,9 @@
 session_start();
 $_SESSION = array();
 session_destroy();
-$_SERVER['PHP_AUTH_USER'] = "";
-$_SERVER['PHP_AUTH_PW'] = "";
+header('Expires: Thu, 21 Jul 1977 07:30:00 GMT'); // When yours truly first set eyes on this world! :)
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache'); // For HTTP/1.0 compatibility
 header("Location: index.php"); // перенаправление на главную страницу после выхода
 ?>
